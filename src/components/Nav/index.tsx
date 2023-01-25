7fbcc72166f7eb81441607f3d292878f8d2a894d
franco <franco.roberti@truenorth.co>
2023-01-25 16:37:55 -0300

    ASD

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,12 +1,27 @@
-import React from "react"
+import React, {useEffect} from "react"
 import styled from "styled-components"
 import Logo from "./logo.svg"
 import colors from "@/constants/colors";
 import Link from "next/link";
 import Text from "../Text"
+import sizes from "../../constants/sizes";
 
-const Nav = ({title}) =>
-    <NavContainer>
+const Nav = ({title}) => {
+    useEffect( () => {
+        window.addEventListener("scroll", resizeHeaderOnScroll);
+    }, [] );
+
+    const resizeHeaderOnScroll = () => {
+        if (document.body.scrollTop > sizes.navPxHeight || document.documentElement.scrollTop > sizes.navPxHeight) {
+            document.getElementById("navbar").style.backgroundColor = colors.secondary;
+            document.getElementById("navbar").style.borderBottom = `1px solid ${colors.primary}`;
+        } else {
+            document.getElementById("navbar").style.backgroundColor = "black";
+            document.getElementById("navbar").style.borderBottom = `none`;
+        }
+    }
+
+    return <NavContainer id="navbar">
         <Link href={`/`}>
             <Logo width={150} height={40}/>
         </Link>
@@ -16,9 +31,12 @@ const Nav = ({title}) =>
             /*<MenuIcon fill="white" height={24} width={24}/>*/
         }
     </NavContainer>
+}
 
 const DummyDiv = styled.div`
+  display: none;
   @media screen and (min-width: 800px) {
+    display: block;
     width: 150px;
   }
 `;
@@ -29,13 +47,13 @@ const NavContainer = styled.div`
   justify-content: space-between;
   display: flex;
   flex-direction: row;
-  background-color: ${colors.secondary};
+  background-color: black;
   align-items: center;
   position: fixed;
   width: calc(100% - 32px);
   top: 0;
   z-index: 10;
-  border-bottom: 1px solid ${colors.primary};
+  transition: background-color 1s;
 `;
 
 export default Nav
